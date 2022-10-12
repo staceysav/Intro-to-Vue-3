05b78f41ea96261248680c9011d1d02c3c4b95b3
staceysav <nastasia.savinova@gmail.com>
2022-10-12 13:34:52 +0300

    initial commit

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            cart: 0,
+            product: 'Socks',
+            description: 'green color, comfortable fit',
+            image: './assets/images/socks_green.jpg',
+            onSale: true,
+            sizes: [1, 2, 3],
+            colors: [{ id: 123, color: "black", quantity: 50 }, { id: 1234, color: "yellow", quantity: 0 }],
+            brand: "Vue mastery",
+        }
+    },
+// Adding method that we can use
+    methods: {
+        addToCart() {
+            this.cart +=1 //addressing cart in the data object above
+        },
+        descrementCart() {
+            this.cart -=1
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + " " + this.product
+        }
+    }
+})
